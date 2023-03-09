271139b0ee38c7b5fb8081644d72120e20b765c7
robertwilliampytel <125312774+robertwilliampytel@users.noreply.github.com>
2023-03-08 23:56:04 -0600

    Add files via upload

---
+++ hw.f95
@@ -0,0 +1,14 @@
+! Hello World in Fortran
+program first
+
+implicit none
+integer :: radius, diameter
+integer :: height=100, width=150
+real :: area, perimeter
+real :: length = 123.5, distance=413.761
+real, parameter :: pi = 3.14159
+character(11) :: msg = "Hello World"
+
+    write (*,*) "Greeting: ", msg
+
+end program first
